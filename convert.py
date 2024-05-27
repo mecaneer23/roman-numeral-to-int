8c0dd34483ce2813382314cb0da494ace97f8f5e
Mecaneer23 <74385377+Mecaneer23@users.noreply.github.com>
2024-05-27 14:18:43 -0500

    refactor: rename variables and add simple tests

--- convert.py
+++ convert.py
@@ -1,16 +1,28 @@
-def roman_to_int(self, s: str) -> int:
-    roman_numerals = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000,
-    }
+"""Convert a roman numeral string to an integer"""
+
+ROMAN_NUMERALS = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
+}
+
+
+def roman_to_int(roman: str) -> int:
+    """Convert a roman numeral string to an integer"""
     total = 0
-    for i in range(len(s)):
-        if i != 0 and roman_numerals[s[i - 1]] < roman_numerals[s[i]]:
-            total -= roman_numerals[s[i - 1]] * 2
-        total += roman_numerals[s[i]]
+    for i, _ in enumerate(roman):
+        if i != 0 and ROMAN_NUMERALS[roman[i - 1]] < ROMAN_NUMERALS[roman[i]]:
+            total -= ROMAN_NUMERALS[roman[i - 1]] * 2
+        total += ROMAN_NUMERALS[roman[i]]
     return total
+
+
+assert roman_to_int("III") == 3
+assert roman_to_int("XIV") == 14
+assert roman_to_int("XCVII") == 97
+assert roman_to_int("MCMLXXVI") == 1976
+print("all tests passed!")
